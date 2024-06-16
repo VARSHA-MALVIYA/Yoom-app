2f288462f90564148dc770e89ac939e14393442c
VARSHA MALVIYA <140935808+VARSHA-MALVIYA@users.noreply.github.com>
2024-06-16 16:16:42 +0530

    Update MeetingTypeList.tsx

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -67,7 +67,7 @@ const MeetingTypeList = () => {
 
   if (!client || !user) return <Loader />;
 
-  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetail?.id}`;
+  const meetingLink = `https://yoomapp.netlify.app/meeting/${callDetail?.id}`;
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
